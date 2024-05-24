390707bd1565ba9b7ddd7b52e0cf8aaa92cd6cba
WangariNelly <nellywangari30@gmail.com>
2024-05-24 16:18:54 +0300

    nav toggle event listeners

--- app.js
+++ app.js
@@ -11,3 +11,21 @@
 
 // ********** smooth scroll ************
 // select links
+
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+const linksContainer = docuent.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+const navbarToggle = document.querySelector('.nav-toggle').addEventListener('click', () => {
+    const linksHeight = links.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+      linksContainer.style.height = `${linksHeight}px`;
+    } else {
+      linksContainer.style.height = 0;
+    }
+});
+
+
